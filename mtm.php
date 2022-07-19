de601b602baaf609976994fff780f19aa5c25631
KakashiPX <106098213+KakashiPX@users.noreply.github.com>
2022-07-19 20:20:40 +0200

    Add files via upload

---
+++ mtm.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<head>
+<title>Manges t morts </title>
+<body>
+<h1><strong>Bienvenue sur le site manges t'es morts !</strong></h1>
+
+<?php
+$lines = 1;
+
+while ($lines <= 50000)
+{
+    echo 'Manges t\'es morts   numéro: ' . $lines . '<br />';
+    $lines++;
+}
+?>
+
+
+</body>
+</html>
+
+</body>
+</head>
